263dd2e59452bcd5674a970c6d8d75de51eb02f8
insokem456-glitch <insokem456@gmail.com>
2026-09-07 21:01:43 +0530

    Delete venture-construction-app directory

--- venture-construction-app/scratch/add_referrer.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/class="slider-img"/g, 'class="slider-img" referrerpolicy="no-referrer"');
-fs.writeFileSync('index.html', html);
-console.log('Added referrerpolicy="no-referrer"');
--- venture-construction-app/scratch/remove_lazy.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace(/ loading="lazy"/g, '');
-fs.writeFileSync('index.html', html);
-console.log('Removed loading="lazy"');
